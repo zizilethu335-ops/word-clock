81fc460faec3327c1d45b631ddc0ac88e541e4b9
zizilethu335-ops <zizilethu335@gmail.com>
2026-01-30 18:13:43 +0200

    added functionality for the select

--- src/script.js
+++ src/script.js
@@ -18,3 +18,27 @@ londonTimeElement.innerHTML = londonTimezone.format("h:mm:ss [<small>]A[</small>
 }
 updateClocks();
 setInterval(updateClocks, 1000);
+
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+ let cityName = cityTimeZone.split("/")[1].replaceAll("_", " ");
+
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#city");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+
+let citiesSelectElement = document.querySelector("#city-select");
+citiesSelectElement.addEventListener("change", updateCity);
